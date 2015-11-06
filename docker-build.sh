74d658416a1669d045525a797212abf98e33f908
Vladimír Kriška <ujovlado@gmail.com>
2015-11-06 02:13:07 +0100

    Updated helper scripts

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker build -t webelement/website .
+docker build -t webelement/data -f ./.docker/data .
+docker build -t webelement/website -f ./.docker/website .
--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run -i -t --rm -v "$PWD:/jekyll" -w /jekyll -p 4000:4000 webelement/website:latest sh -c 'composer install && npm install && gulp clean && gulp'
+if [[ ! `docker ps --all | grep "webelement-data"` ]]; then
+  docker run --name webelement-data webelement/data
+fi
+
+docker run --volumes-from webelement-data -i -t --rm -v "$PWD:/data" -w /data -p 4000:4000 webelement/website:latest sh -c 'bower install --allow-root && npm install && gulp clean && gulp'
