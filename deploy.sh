d52ac327987281686ea6088c6d4b9f8d2fe897b2
Vladimír Kriška <ujovlado@gmail.com>
2015-11-11 13:14:49 +0100

    Deploy through sshfs

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-scp -r _build webelement.sk@webelement.sk:/new
-echo "rename web web-`date +"%Y%m%d-%H%M%S"`" | sftp -C webelement.sk@webelement.sk
-echo "rename new web" | sftp -C webelement.sk@webelement.sk
+DATETIME=`date +"%Y%m%d-%H%M%S"`
+MOUNT_DIR="sshfs-mount-webelement.sk-$DATETIME"
+
+mkdir -p $MOUNT_DIR
+sshfs webelement.sk@webelement.sk:/ $MOUNT_DIR
+
+if [[ `mount | grep "webelement.sk@webelement.sk"` ]]; then
+    echo "mounted"
+    rm -rf $MOUNT_DIR/new
+    cp -r _build $MOUNT_DIR/new
+    mkdir -p $MOUNT_DIR/web
+    mv $MOUNT_DIR/web $MOUNT_DIR/web-$DATETIME && mv $MOUNT_DIR/new $MOUNT_DIR/web
+    ls -la $MOUNT_DIR
+    fusermount -quz $MOUNT_DIR && rmdir $MOUNT_DIR
+else
+    echo "could not mount"
+    exit 1
+fi
