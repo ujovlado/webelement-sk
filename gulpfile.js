2d29c527b1a4c2f12e82aae6232e5a900e3165ba
Vladimír Kriška <ujovlado@gmail.com>
2016-01-28 19:23:18 +0100

    Updated browser-sync. Reorganized gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var less = require('gulp-less');
 var del = require('del');
 var cssnano = require('gulp-cssnano');
 var concatCss = require('gulp-concat-css');
-var browserSync = require('browser-sync');
+var browserSync = require('browser-sync').create();
 var childProcess = require('child_process');
 var rev = require('gulp-rev');
 var revReplace = require('gulp-rev-replace');
@@ -17,8 +17,10 @@ var paths = {
   jekyll: [
     'app/**/*.md',
     'app/**/*.html',
-    'app/**/*.yml',
     'app/**/.htaccess'
+  ],
+  jekyll_yml: [
+    'app/**/*.yml'
   ],
   css: [
     'fontello/build/css/fontello.css',
@@ -39,14 +41,15 @@ gulp.task('clean', function(callback) {
 
 
 gulp.task('watch', function() {
-  gulp.watch(paths.site, ['site']);
+  gulp.watch(paths.site, ['build-site']);
   gulp.watch(paths.jekyll, ['jekyll-incremental']);
+  gulp.watch(paths.jekyll_yml, ['jekyll']);
   gulp.watch(paths.css, ['css']);
   gulp.watch(paths.fonts, ['fonts']);
 });
 
 gulp.task('jekyll', function(callback) {
-  browserSync.notify('jekyll');
+  browserSync.notify('jekyll build standard');
   childProcess.spawn('jekyll', ['build', '-d', '../_site'], {
     cwd: './app',
     stdio: 'inherit'
@@ -55,7 +58,7 @@ gulp.task('jekyll', function(callback) {
 });
 
 gulp.task('jekyll-incremental', function(callback) {
-  browserSync.notify('jekyll');
+  browserSync.notify('jekyll build incremental');
   childProcess.spawn('jekyll', ['build', '-d', '../_site', '-I'], {
         cwd: './app',
         stdio: 'inherit'
@@ -66,6 +69,10 @@ gulp.task('jekyll-incremental', function(callback) {
 gulp.task('fonts', function() {
   return gulp.src(paths.fonts)
     .pipe(gulp.dest('_build/font'));
+});
+
+gulp.task('build-site', ['minify'], function() {
+  browserSync.reload();
 });
 
 gulp.task('css', function() {
@@ -82,14 +89,16 @@ gulp.task('css', function() {
 });
 
 gulp.task('site', function() {
-  var htmlFilter = filter('**/*.html', {restore: true});
   return gulp.src(paths.site)
-    .pipe(htmlFilter)
+    .pipe(gulp.dest('_build'));
+});
+
+gulp.task('minify', ['site'], function() {
+  return gulp.src('_build/**/*.html')
     .pipe(htmlmin({
       collapseWhitespace: true,
       conservativeCollapse: true
     }))
-    .pipe(htmlFilter.restore)
     .pipe(gulp.dest('_build'));
 });
 
@@ -109,16 +118,12 @@ gulp.task("revreplace", [], function() {
     .pipe(gulp.dest('_build'))
 });
 
-gulp.task('browser-sync', [], function() {
-  browserSync({
-    server: {
-      baseDir: '_build'
-    },
+gulp.task('browser-sync', ['css', 'fonts', 'sprite', 'build-from-site'], function() {
+  browserSync.init({
+    server: '_build',
     host: "localhost",
     port: 4000
   });
 });
 
-gulp.task('default', ['jekyll', 'css', 'fonts', 'sprite'], function() {
-  gulp.start('site', 'watch', 'browser-sync');
-});
+gulp.task('default', ['watch', 'browser-sync']);
