d3c047df58078ba79a0bb4be50d4eb5f806e1bb6
Vladimír Kriška <ujovlado@gmail.com>
2016-01-28 17:42:17 +0100

    Updated locations in sprite task

--- gulpfile.js
+++ gulpfile.js
@@ -94,12 +94,12 @@ gulp.task('site', function() {
 });
 
 gulp.task('sprite', function () {
-  var spriteData = gulp.src(['images/speakers/*.*']).pipe(spritesmith({
+  var spriteData = gulp.src(['app/images/speakers/*.*']).pipe(spritesmith({
     imgName: 'sprite.png',
     cssName: 'sprite.css'
   }));
   return spriteData.pipe(imagemin())
-    .pipe(gulp.dest('images/speakers/sprite'));
+    .pipe(gulp.dest('app/images/speakers/sprite'));
 });
 
 gulp.task("revreplace", [], function() {
