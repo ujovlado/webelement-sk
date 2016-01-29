62455ebdfd4a13f4e757eef86f0cfed709aee33b
Vladimír Kriška <ujovlado@gmail.com>
2016-01-29 15:15:42 +0100

    Update build script to reflect actual gulpfile

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ if [[ ! `docker ps --all | grep "webelement-data"` ]]; then
   docker run --name webelement-data webelement/data:latest chown -R `id -u`:`id -g` /data/node_modules /data/bower_components
 fi
 
-docker run -u `id -u` --volumes-from webelement-data --rm -v "$PWD:/data" -w /data webelement/website:latest sh -c 'bower install && npm install && gulp clean && gulp sprite && JEKYLL_ENV=production gulp jekyll && gulp css && gulp fonts && gulp minify && gulp revreplace'
+docker run -u `id -u` --volumes-from webelement-data --rm -v "$PWD:/data" -w /data webelement/website:latest sh -c 'bower install && npm install && gulp clean && JEKYLL_ENV=production gulp jekyll && gulp copy-app-images && gulp css && gulp fonts && gulp copy-site-files && gulp copy-site-html-and-minify && gulp revreplace'
