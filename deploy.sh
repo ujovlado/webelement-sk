50ece6ca6db059852c9810f935daae8200a8c9e1
Vladimír Kriška <ujovlado@gmail.com>
2016-02-06 20:21:21 +0100

    Updated build and deploy scripts. Gitignore

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-DATETIME=`date +"%Y%m%d-%H%M%S"`;
+DATETIME=`date +"%Y%m%d-%H%M%S"`
+ARCHIVE="build-$DATETIME.tar.gz"
+SCRIPT="build-$DATETIME.sh"
 
-scp -r _build webelement.sk@webelement.sk:/new-$DATETIME
-echo -e "rename web web-$DATETIME\nrename new-$DATETIME web" | sftp -C webelement.sk@webelement.sk
+mv _build.tar.gz $ARCHIVE
+touch $SCRIPT && chmod +x $SCRIPT
+
+echo "#!/bin/bash
+cd ~/web/webelement.sk
+rm -rf _build
+echo \"Extracting archive ...\"
+tar -xzf $ARCHIVE
+mv _build webelement.sk-$DATETIME
+rm web && ln -s webelement.sk-$DATETIME web
+" > $SCRIPT
+
+scp -r $ARCHIVE $SCRIPT webelement@webelement.sk:~/web/webelement.sk
+ssh webelement@webelement.sk "~/web/webelement.sk/$SCRIPT"
