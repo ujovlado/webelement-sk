7ec8132730e27d819c0e5b16fbe73c4e7ac2c1e0
Vladimír Kriška <ujovlado@gmail.com>
2016-03-27 23:32:35 +0200

    Testing Docker Cloud trigger

--- create-image.sh
+++ create-image.sh
@@ -6,3 +6,5 @@ docker ps -a
 docker login -e="." -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 docker tag webelement/web quay.io/webelement/web:latest
 docker push quay.io/webelement/web:latest
+
+curl -X POST -s https://cloud.docker.com/api/app/v1/service/$DOCKER_CLOUD_SERVICE_ID/trigger/$DOCKER_CLOUD_TRIGGER_ID/call/ -o /dev/null
