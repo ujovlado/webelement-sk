7fd8778ed853bc20b5bccda1d7459bd2dd9631c4
Vladimír Kriška <ujovlado@gmail.com>
2016-03-28 22:24:56 +0200

    Deleted old Docker related files

--- docker-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker build -t webelement/bower-and-node ./docker/bower-and-node \
-&& docker build \
-  -t webelement/website \
-  --build-arg userId=`id -u` \
-  --build-arg groupId=`id -g` \
-  ./docker/website
