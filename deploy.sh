a608d719d2c38cd8461ef251bf8dbfcff55acf4d
Vladimír Kriška <ujovlado@gmail.com>
2016-03-28 22:19:14 +0200

    Renamed deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,5 @@ docker login -e="." -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 docker tag webelement/web quay.io/webelement/web:latest
 docker push quay.io/webelement/web:latest
 
+echo "Redeploying service ..."
 curl -X POST -s https://cloud.docker.com/api/app/v1/service/$DOCKER_CLOUD_SERVICE_ID/trigger/$DOCKER_CLOUD_TRIGGER_ID/call/ -o /dev/null
