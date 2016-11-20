80d1ad5b450229bece52d159164b3ab1d8e1c576
Vladimír Kriška <ujovlado@gmail.com>
2016-11-20 20:44:22 +0100

    Remove build from deploy script. Remove -e from login.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-docker build -t webelement/web .
-docker ps -a
-
-docker login -e="." -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
+docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 docker tag webelement/web quay.io/webelement/web:latest
 docker push quay.io/webelement/web:latest
 
