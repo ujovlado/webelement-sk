6c65ce5b95bce72cbb01f743e582c70219846ac8
Vladimír Kriška <ujovlado@gmail.com>
2016-12-29 23:01:35 +0100

    Remove image building in build script

--- build.sh
+++ build.sh
@@ -4,5 +4,4 @@ docker-compose run --rm node \
   && cp -R fontello/build/font/ app/ \
   && cp -R bower_components/bootstrap-sass/assets/stylesheets/ app/_sass \
   && cp fontello/build/css/fontello.css app/css/ \
-  && docker-compose run --rm -e JEKYLL_ENV=production jekyll jekyll build -s ./app -d _build \
-  && docker-compose build app
+  && docker-compose run --rm -e JEKYLL_ENV=production jekyll jekyll build -s ./app -d _build
