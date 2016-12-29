f74e78b92bbf91610f10c477a4a53a5653db88f5
Vladimír Kriška <ujovlado@gmail.com>
2016-12-29 20:22:43 +0100

    Remove Docker Cloud trigger from deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,3 @@ docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io \
   && docker tag webelement/web quay.io/webelement/web:latest \
   && docker images \
   && docker push quay.io/webelement/web:latest \
-  && test -z "`curl -X POST https://cloud.docker.com/api/app/v1/service/$DOCKER_CLOUD_SERVICE_ID/trigger/$DOCKER_CLOUD_TRIGGER_ID/call/`"
