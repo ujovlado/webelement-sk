bbabdb3dd9800ba0e9355721d384f01324fddabb
Vladimír Kriška <ujovlado@gmail.com>
2017-02-12 14:27:41 +0100

    Revert "Add region to deploy script"
    This reverts commit cd6831f3c482943574775be2e94130da808c89ce.

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 docker-compose run --rm \
   -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
   -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-  aws --region eu-central-1 s3 rm s3://webelement.sk --recursive
+  aws s3 rm s3://webelement.sk --recursive
 
 docker-compose run --rm \
   -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
   -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-  aws --region eu-central-1 s3 cp _build s3://webelement.sk --recursive --acl "public-read" --cache-control "max-age=600"
+  aws s3 cp _build s3://webelement.sk --recursive --acl "public-read" --cache-control "max-age=600"
