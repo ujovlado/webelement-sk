9a7a16405ab9b19d049b5ab761937c9576769577
Vladimír Kriška <ujovlado@gmail.com>
2017-02-12 14:28:02 +0100

    Revert "Change deploy script to push files to s3"
    This reverts commit dccc0ea291133a315c13d907b1e77cd013524b2c.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-docker-compose run --rm \
-  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-  -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-  aws s3 rm s3://webelement.sk --recursive
-
-docker-compose run --rm \
-  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-  -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-  aws s3 cp _build s3://webelement.sk --recursive --acl "public-read" --cache-control "max-age=600"
+docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io \
+  && docker tag webelement/web quay.io/webelement/web:latest \
+  && docker images \
+  && docker push quay.io/webelement/web:latest \
