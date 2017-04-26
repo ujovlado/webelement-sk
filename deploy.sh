1eff16e70745eefb8133fa1867d97750000ed384
Vladimír Kriška <ujovlado@gmail.com>
2017-04-26 22:39:33 +0200

    Push also to ECR

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,12 @@ docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io \
   && docker tag webelement/webelement-web quay.io/webelement/webelement-web:latest \
   && docker images \
   && docker push quay.io/webelement/webelement-web:latest \
+  && docker logout \
   && test -z "`curl -X POST https://cloud.docker.com/api/app/v1/service/$DOCKER_CLOUD_SERVICE_ID/trigger/$DOCKER_CLOUD_TRIGGER_ID/call/`"
+
+docker run -i -t --rm \
+  -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
+  -e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
+  quay.io/rekurzia/aws-cli-docker ecr get-login --region eu-central-1 | tr -d "\r\n" | sh \
+  && docker tag webelement/webelement-web "$AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/webelement/web:latest" \
+  && docker push "$AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/webelement/web:latest"
