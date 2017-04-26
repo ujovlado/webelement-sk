ce03cf8b8ea04cb8aa8463ac5fe80931b5d56791
Vladimír Kriška <ujovlado@gmail.com>
2017-04-26 22:54:54 +0200

    Use account id directly

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,5 @@ docker run -i -t --rm \
   -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
   -e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
   quay.io/rekurzia/aws-cli-docker ecr get-login --region eu-central-1 | tr -d "\r\n" | sh \
-  && docker tag webelement/webelement-web "$AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/webelement/web:latest" \
-  && docker push "$AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/webelement/web:latest"
+  && docker tag webelement/webelement-web 297411564328.dkr.ecr.eu-central-1.amazonaws.com/webelement/web:latest \
+  && docker push 297411564328.dkr.ecr.eu-central-1.amazonaws.com/webelement/web:latest
