fc2aab62721c79351e16c77855e4561fcb449cdd
Vladimír Kriška <ujovlado@gmail.com>
2017-05-06 10:49:40 +0200

    Test nginx conf

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ docker-compose run --rm node \
   && cp -R bower_components/bootstrap-sass/assets/stylesheets/ app/_sass \
   && cp fontello/build/css/fontello.css app/css/ \
   && docker-compose run --rm -e JEKYLL_ENV=production jekyll jekyll build -s ./app -d _build \
-  && docker-compose build app
+  && docker-compose build app \
+  && docker-compose run --rm app nginx -t
