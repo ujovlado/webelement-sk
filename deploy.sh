813dc621327ecdf2b6f9c6ad59dbd64482069928
Vladimír Kriška <ujovlado@gmail.com>
2017-05-06 10:44:03 +0200

    Remove deploy to quay and docker cloud

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io \
-  && docker tag webelement/webelement-web quay.io/webelement/webelement-web:latest \
-  && docker images \
-  && docker push quay.io/webelement/webelement-web:latest \
-  && docker logout \
-  && test -z "`curl -X POST https://cloud.docker.com/api/app/v1/service/$DOCKER_CLOUD_SERVICE_ID/trigger/$DOCKER_CLOUD_TRIGGER_ID/call/`"
-
 docker run -i -t --rm \
   -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
   -e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
