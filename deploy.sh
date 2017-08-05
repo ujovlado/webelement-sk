3a50980e6d4faed0e26eb236a9005200025c35f7
Vladimír Kriška <ujovlado@gmail.com>
2017-08-05 15:12:41 +0200

    Deploy to s3

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-docker run -i -t --rm \
-  -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
-  -e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
-  quay.io/rekurzia/aws-cli-docker ecr get-login --region eu-central-1 | tr -d "\r\n" | sh \
-  && docker tag webelement/webelement-web 297411564328.dkr.ecr.eu-central-1.amazonaws.com/webelement/web:latest \
-  && docker push 297411564328.dkr.ecr.eu-central-1.amazonaws.com/webelement/web:latest
+docker-compose run --rm \
+  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+  -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+  aws --region eu-central-1 s3 rm s3://www.webelement.sk --recursive
+
+docker-compose run --rm \
+  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+  -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+  aws --region eu-central-1 s3 cp _build s3://www.webelement.sk --recursive --acl "public-read" --cache-control "max-age=600"
+
+docker-compose run --rm \
+  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+  -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+  aws --region eu-central-1 s3api put-bucket-website --bucket www.webelement.sk --website-configuration file://provisioning/website.json
