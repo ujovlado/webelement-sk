31d391d870b54f9f1d657d7a979ed5c772251e58
Vladimír Kriška <vlado@rekurzia.com>
2020-04-29 08:57:21 +0200

    Remove old shell scripts

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker-compose run --rm node \
-  && cp -R fontello/build/font/ app/ \
-  && cp -R bower_components/bootstrap-sass/assets/stylesheets/ app/_sass \
-  && cp fontello/build/css/fontello.css app/css/ \
-  && docker-compose run --rm -e JEKYLL_ENV=production jekyll jekyll build -s ./app -d _build
